cf856e2c897aa4f1508b3fe6d124bf5b876cd30f
A-Wu5 <roboanthony@gmail.com>
2023-08-24 15:48:07 -0700

    Added carousel component

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,25 @@
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+import { Card } from "Card";
+
+const Carousel = ({ cards }) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex gap-4">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Carousel;
