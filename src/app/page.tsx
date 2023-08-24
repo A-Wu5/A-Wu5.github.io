67d8827637c5d15bf5a3a69848adf7a6b7006569
A-Wu5 <roboanthony@gmail.com>
2023-08-24 15:01:37 -0700

    Added Linkedin logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,26 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Links{" "}
+            Github{" "}
             <Image
               src="/github-mark/github-mark.png"
               alt="Github link"
+              className="dark:invert"
+              width={28}
+              height={28}
+              priority
+            />
+          </a>
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://www.linkedin.com/in/anthonywu5/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin{" "}
+            <Image
+              src="/linkedin-mark/linkedin-mark.png"
+              alt="Linkedin link"
               className="dark:invert"
               width={28}
               height={28}
@@ -29,14 +45,7 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <p className="justify-center border-bd text-5xl">Anthony Wu</p>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
